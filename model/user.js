81c0e1614db3cca0fa26dddff12ae66b74df3e5d
KKDAY <kkday@KKDAYde-MacBook-Air-5.local>
2017-09-23 13:02:23 +0800

    userapi

---
+++ model/user.js
@@ -0,0 +1,12 @@
+
+var mongoose = require('mongoose');
+
+var users = [{  
+    id: 1,
+    name: "Nicki",
+    email: "nicki8819@gmail.com",
+    password: "abcd1234"
+}
+
+
+module.exports = users;  
--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var app = express(); // define our app using express
 var bodyParser = require('body-parser');
 var router = express.Router();
 
+
+var auth = require('./controllers/auth.js')();
 var mongoose = require ('mongoose');
 mongoose.connect('mongodb://Nicki:n800829@ds051990.mlab.com:51990/apiproject')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 
 router.get('/',function(req,res){
 	res.json({message:"welcome Back"});
@@ -15,6 +18,7 @@ router.get('/',function(req,res){
 });
 
 var courseController = require('./controllers/course.js')
+var userController = require('./controllers/user.js')
 
 router.route('/courses')
 .post(courseController.postCourse)
@@ -22,6 +26,9 @@ router.route('/courses')
 
 router.route('/courses/:id')
 .get(courseController.getCourseById)
+
+router.route('/login')
+.post(userController.getUserToken)
 
 app.use('/',router);
 app.listen (process.env.PORT || 8080);
